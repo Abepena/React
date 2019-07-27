02b43aed5b5a69967bd7120ce1fb5ee074c1c257
Abraham Pena <abe@Abrahams-MacBook-Pro.local>
2019-07-27 13:44:17 -0400

    v2

--- contact_manager/src/components/Contact.js
+++ contact_manager/src/components/Contact.js
@@ -8,8 +8,12 @@ class Contact extends Component {
     };
 
     state = {
-        showContactInfo: false
-    };
+        showContactInfo: false,
+		};
+		
+		onDeleteClick = () => {
+			this.props.deleteClickHandler();
+		}
 
     render() {
         const { name, phone, email } = this.props.contact;
@@ -26,26 +30,20 @@ class Contact extends Component {
                         }
                         className="fas fa-sort-down"
                     />
-                    <i
-                        className="fas fa-times"
-                        onClick={console.log("clicked")}
-                    />
+                    <i 
+											className="fas fa-times"
+											onClick={this.onDeleteClick}
+										>	
+										</i>
                 </h4>
                 {showContactInfo ? (
                     <ul className="list-group">
                         <li className="list-group-item">{email}</li>
                         <li className="list-group-item">{phone}</li>
                     </ul>
-                ) : null}
+                ) : null }
             </div>
         );
     }
 }
-
-// Contact.propTypes = {
-//     name: PropTypes.string.isRequired,
-//     email: PropTypes.string.isRequired,
-//     phone: PropTypes.string.isRequired,
-// };
-
 export default Contact;
